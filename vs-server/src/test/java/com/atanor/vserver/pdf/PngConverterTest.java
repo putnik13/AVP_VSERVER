d007216d9daa257f1c9bf46758b2207a632cace1
Mykola Koval <mykola_koval@epam.com>
2013-12-10 18:32:37 +0200

    additional changes to pdf logo

--- vs-server/src/test/java/com/atanor/vserver/pdf/PngConverterTest.java
+++ vs-server/src/test/java/com/atanor/vserver/pdf/PngConverterTest.java
@@ -1,22 +1,29 @@
 package com.atanor.vserver.pdf;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
 import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfStamper;
 import com.lowagie.text.pdf.PdfWriter;
 import com.lowagie.text.pdf.codec.PngImage;
 
 public class PngConverterTest {
 
 	private static final Rectangle PAGE_FORMAT = PageSize.A4.rotate();
-	
+
 	private static final String PDF_FULL_NAME = "d:/temp/pdf/ConvertImagetoPDF.pdf";
+	private static final String PDF_WITH_LOGO_FULL_NAME = "d:/temp/pdf/ConvertImagetoPDFLogo.pdf";
 	private static final String IMAGE1_FULL_NAME = "d:/temp/pdf/test0.png";
 	private static final String IMAGE2_FULL_NAME = "d:/temp/pdf/test1.png";
 	private static final String IMAGE3_FULL_NAME = "d:/temp/pdf/test2.png";
+	private static final String LOGO_FULL_NAME = "d:/tmp/pdf/testlogo.png";
 	
 	public static void main(String[] args) {
 		try {
@@ -39,15 +46,32 @@ public class PngConverterTest {
 			convertPngToPdf.add(convertBmp2);
 
 			convertPngToPdf.close();
-			
+
 		} catch (Exception i1) {
 			i1.printStackTrace();
 		}
 	}
-	
+
 	private static void setImagePosition(final Image image) {
-		image.setAbsolutePosition(0,0);
+		image.setAbsolutePosition(0, 0);
 		image.scaleAbsolute(PAGE_FORMAT.getWidth(), PAGE_FORMAT.getHeight());
+	}
+
+	public static void addLogo() throws IOException, DocumentException {
+		PdfReader reader = new PdfReader(PDF_FULL_NAME);
+		PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(PDF_WITH_LOGO_FULL_NAME));
+
+		Image logoImage = PngImage.getImage(LOGO_FULL_NAME);
+		logoImage.setAbsolutePosition(20, PAGE_FORMAT.getHeight() - 20 - 50);
+		logoImage.scaleAbsolute(50, 50);
+		
+		for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+			final PdfContentByte over = stamper.getOverContent(i);
+			over.addImage(logoImage);
+		}
+		
+		stamper.close();
+		reader.close();
 	}
 
 }
