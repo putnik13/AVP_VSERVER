7ce1ca14b4063f017501e5b5ced52c00f6f2ebc9
Mykola Koval <mykola_koval@epam.com>
2014-01-17 17:49:45 +0200

    drawing changes

--- vs-client-ui/src/main/java/com/atanor/vserver/vsclient/client/ui/widgets/BackgroundCanvas.java
+++ vs-client-ui/src/main/java/com/atanor/vserver/vsclient/client/ui/widgets/BackgroundCanvas.java
@@ -1,6 +1,9 @@
 package com.atanor.vserver.vsclient.client.ui.widgets;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
+import com.google.gwt.user.client.Event.NativePreviewHandler;
 import com.google.gwt.user.client.Window;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.Cursor;
@@ -8,8 +11,13 @@ import com.smartgwt.client.types.Overflow;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.Img;
 import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.drawing.DrawOval;
+import com.smartgwt.client.widgets.drawing.DrawPane;
+import com.smartgwt.client.widgets.drawing.Point;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.events.MouseMoveEvent;
+import com.smartgwt.client.widgets.events.MouseMoveHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.LayoutSpacer;
 import com.smartgwt.client.widgets.layout.VLayout;
@@ -74,9 +82,59 @@ public class BackgroundCanvas extends Canvas {
 		backgroundImg.setWidth100();
 		backgroundImg.setHeight100();
 
-		addChild(pdfCanvas);
-		addChild(backgroundImg);
-		addChild(labelLayout);
+		// addChild(pdfCanvas);
+		// addChild(backgroundImg);
+		// addChild(labelLayout);
+
+		final DrawPane drawPane = new DrawPane();
+		drawPane.addMouseMoveHandler(new MouseMoveHandler() {
+			
+			@Override
+			public void onMouseMove(MouseMoveEvent event) {
+				final int eventX = event.getX();
+				final int eventY = event.getY();
+				 final DrawOval drawOval = new DrawOval(); 
+	                drawOval.setDrawPane(drawPane);
+	                drawOval.setCenterPoint(new Point(eventX, eventY));
+	                drawOval.setRadius(20);
+	                drawOval.setFillColor("red");
+	                drawOval.setLineColor("red");
+	                drawOval.show();
+			}
+		});
+		
+		
+//		Event.addNativePreviewHandler(new NativePreviewHandler() {
+//			public void onPreviewNativeEvent(final NativePreviewEvent event) {
+//				
+//				final int eventX = event.getNativeEvent().getClientX();
+//				final int eventY = event.getNativeEvent().getClientY();				
+//				final int eventType = event.getTypeInt();
+//				
+//				switch (eventType) {
+//				case Event.ONMOUSEMOVE:
+//					  
+//	                final DrawOval drawOval = new DrawOval(); 
+//	                drawOval.setDrawPane(drawPane);
+//	                drawOval.setCenterPoint(new Point(eventX, eventY));
+//	                drawOval.setRadius(20);
+//	                drawOval.setFillColor("red");
+//	                drawOval.setLineColor("red");
+//	                drawOval.show();
+//	                
+//					break;
+//				case Event.ONCLICK:
+//					Window.alert("Clicked @ " + eventX + "," + eventY);
+//					break;
+//				default:
+//					// not interested in other events
+//				}
+//			}
+//		});
+
+		drawPane.setWidth100();
+		drawPane.setHeight100();
+		addChild(drawPane);
 	}
 
 	private void setLabelContent(final String content) {
